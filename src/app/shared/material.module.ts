7ece2a483775210ef4d069857a9a5aff6696e2c7
ZEvans <zachevans@live.com>
2018-02-13 21:49:33 -0600

    Added AngularMaterial and hammerjs to application. Created Module for Material Component imports. Ask me if you do not know what these are. Imported Material.module into app.module.

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatCheckboxModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
+  declarations: []
+})
+export class MaterialModule { }
